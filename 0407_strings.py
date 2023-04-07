fa47d49b4840e5a44df4cb7f387bf6055d21e686
Sanjay Patel <sanjaypatel16@gmail.com>
2023-04-07 12:03:10 -0400

    fist commit

--- 0407_strings.py
+++ 0407_strings.py
@@ -1,4 +1,12 @@
 #Today we are learning about Strings
-fruits = "Apples"
+fruits = ["Apples", "banana", "grapes", "strawberry", "chikoo", "blackberry"]
+fruits.append("cherry")
+print(fruits)
 
-print(fruits[5])
+def replace_email(email, old_domain, new_domain):
+    if "@" + old_domain in email:
+        index = email.index("@"+old_domain)
+        new_email=email[:index]+"@"+new_domain
+        print(index)
+    return email
+
